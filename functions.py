018338cc39137b5d7ef6fc4591729cc32c6f38b0
MGroening <maximilian.groening@gmx.de>
2024-10-17 21:40:19 +0200

    currenty fucked

--- functions.py
+++ functions.py
@@ -1,43 +1,106 @@
 # functions file for scripts
 
+# import packages
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import truncnorm
+
+# FUNCTIONS
+
+def generate_positive_normal(mean, std_dev):
+    # Set lower limit to 0, and use the standard deviation and mean for the normal distribution
+    a = 0  # lower limit
+    b = np.inf  # no upper limit
+
+    # Calculate the truncation
+    lower_limit = (a - mean) / std_dev
+    upper_limit = (b - mean) / std_dev
+
+    # Generate a random number from the truncated normal distribution
+    return truncnorm.rvs(lower_limit, upper_limit, loc=mean, scale=std_dev)
 
 
 
-
-# creation of vector for weekly data of company 
-# takes variables and returns vector
-# (week, order_suppl, amt_transp, amt_wip, amt_stock, 
-# cycle_stock, safety_stock, order_cust, blog_cust, demand_cust)
-def create_new(week, order_suppl, amt_transp, amt_wip, amt_stock, cycle_stock, safety_stock, order_cust, blog_cust, demand_cust):
-    
-    v = [week, order_suppl, amt_transp, amt_wip, amt_stock, cycle_stock, safety_stock, order_cust, blog_cust, demand_cust]
-    
-    return v
 
 
 # save the vector data into matrix for KPI
-def save_into_matrix(matrix, vector):
-    matrix.append(vector.copy())
-    return matrix
+def save_into_matrix(m_list, vector, v_list):
+    m_list[v_list.index(vector)].append(vector.copy())
+    return 
+   
+
+# calculate demand of customer (order current week + backlog)
+def calc_demand_cust(vector):
+    vector[9] = vector[7] + vector[8]
+    return vector
 
 
 # calculate backlog for company from place 7 and 8 of company vector
 def calc_blog(vector):
-
+    if vector[4] < vector[9]:
+        vector[8] += (vector[9] - vector[4])
     return vector
-    
 
 # move products from wip into stock
 def move_to_stock(vector):
-    vector[4] = vector[4] + vector[3]
+    vector[4] += vector[3]
     return vector
 
 
 # move products from transport into wip
 def move_to_wip(vector):
     vector[3] = vector[2]
+    vector[2] = 0
     return vector
 
+# checks if brewery: order will be next in transport to brewery, 
+# else: moves order_suppl of next in line into order_cust of current company
+def pass_order(vector, v_list):
+    if v_list.index(vector) == 0:
+        v_list[0][3] = v_list[0][2]
+    elif v_list.index(vector) == 3:
+        pass
+    else:
+        v_list[v_list.index(vector)+1][7] = v_list[v_list.index(vector)][1]
+
+# calculate delivery amount
+def calc_delivery(vector):
+    amt_stock = vector[4]
+    demand_cust = vector[9]
+
+    if amt_stock >= demand_cust:
+        delivery_amt = demand_cust
+        vector[4] -= demand_cust
+        if vector[8] > 0:
+            vector[8] -= delivery_amt
+            vector[9] -= delivery_amt
+        else:
+            vector[9] -= delivery_amt
+    else:
+        
+        delivery_amt = amt_stock
+        vector[4] = 0 
+        if vector[8] > 0:
+            vector[8] -= delivery_amt
+            vector[9] -= delivery_amt
+        else:
+            vector[9] -= delivery_amt
+
+    return delivery_amt
+
+
+# dispatch products to transport in direction of customer
+def move_to_transp(vector, v_list, del_amt):
+    # if bar add to delivered_cust
+    if v_list.index(vector) == 3:
+        v_list[v_list.index(vector)][10] += del_amt
+    # else do the same send the amount to amt_transp to next in line
+    else:
+        v_list[v_list.index(vector)][10] += del_amt
+        v_list[v_list.index(vector)+1][2] = del_amt
+
+    return
 
 # change var:week to current
 def change_week(vector, i):
--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 # main file for running simulations
 
 # import packages
-import matplotlib as m 
-import numpy as n
+
+import numpy as np
+import matplotlib.pyplot as plt
 
 # import seperate .py into main
 import functions as f
 
 
-
 # CONSTANTS
+
+# sim constants
+variance = 10
+std_dev = np.sqrt(variance)
+avg_demand =10
+sim_time = 30
+cost_stock = 0.5
+cost_blog = 1
+np.random.seed(42)
+
 # starting conditions
 s_amt_transp = 4
 s_amt_wip = 4
 s_amt_stock = 12
+
 # speculated inventory variables
 s_cycle_stock = 8
 s_safety_stock = 8
@@ -22,12 +33,15 @@ s_safety_stock = 8
 # SIMULATION
 
 # start simulation, define starting vector and start weekly cycle
-def sim(s_amt_transp, s_amt_wip, s_amt_stock):
-    # define starting vector
-    v_brew = [0, 4, s_amt_transp, s_amt_wip, s_amt_stock, s_cycle_stock, s_safety_stock, 4, 0, 4]
-    v_bottl = [0, 4, s_amt_transp, s_amt_wip, s_amt_stock, s_cycle_stock, s_safety_stock, 4, 0, 4]
-    v_wholes = [0, 4, s_amt_transp, s_amt_wip, s_amt_stock, s_cycle_stock, s_safety_stock, 4, 0, 4]
-    v_bar = [0, 4, s_amt_transp, s_amt_wip, s_amt_stock, s_cycle_stock, s_safety_stock, 4, 0, 4]
+def sim():
+    
+    # definition of vectors for weekly data of companys
+    # (week, order_suppl, amt_transp, amt_wip, amt_stock, 
+    # cycle_stock, safety_stock, order_cust, blog_cust, demand_cust, delivered_cust]
+    v_brew = [0, 2, s_amt_transp, s_amt_wip, s_amt_stock, s_cycle_stock, s_safety_stock, 4, 0, 4, 4]
+    v_bottl = [0, 4, s_amt_transp, s_amt_wip, s_amt_stock, s_cycle_stock, s_safety_stock, 4, 0, 4, 4]
+    v_wholes = [0, 6, s_amt_transp, s_amt_wip, s_amt_stock, s_cycle_stock, s_safety_stock, 4, 0, 4, 4]
+    v_bar = [0, 8, s_amt_transp, s_amt_wip, s_amt_stock, s_cycle_stock, s_safety_stock, 4, 0, 4, 0]
 
     # define starting matrix
     m_brew = []
@@ -42,8 +56,14 @@ def sim(s_amt_transp, s_amt_wip, s_amt_stock):
     m_list = (m_brew, m_bottl, m_wholes, m_bar)
 
 
-    # loop for 30 weeks (TEST for 2)
-    for i in range(1, 6):
+    # loop for sim_time
+    for i in range(1, sim_time+1):
+        
+        # calculation of demand with normal distribution
+        demand_ak = int(f.generate_positive_normal(avg_demand, std_dev))
+        v_list[3][7] = demand_ak
+        print(demand_ak)
+
         
         # loop for every company
         for c in v_list:
@@ -53,17 +73,35 @@ def sim(s_amt_transp, s_amt_wip, s_amt_stock):
             # move products from transport into wip
             f.move_to_wip(c)
 
+            # calculate demand and backlog
+            f.calc_demand_cust(c)
+            f.calc_blog(c)
+
+            # calculate delivery amount an move out of 
+            del_amt = f.calc_delivery(c)
+
+            # dispatch order to customer
+            f.move_to_transp(c, v_list, del_amt)
+
+            # pass order_suppl of company next in line into order_cust of current company
+            f.pass_order(c, v_list)
+
             # change var:week to current
             f.change_week(c, i)
-            print(c)
 
             # save vector in matrix
-            for m in m_list:
-                f.save_into_matrix(m, c)
+            f.save_into_matrix(m_list, c, v_list)
+
+            print(f"Week {i}: Order Cust: {c[7]}, Backlog: {c[8]}, Demand: {c[9]}, Stock: {c[4]}")
+
+        print(v_bar)
+    
+    print(m_bar)
+    return
         
 
 
 
 #RUN
 
-sim(s_amt_transp, s_amt_wip, s_amt_stock)
+sim()
